337da25bb54c998d4e48c2aa5116a7f4b40c0447
wuyutao <wuyutaoktm@163.com>
2025-03-14 13:36:27 -0700

    add Header and Footer

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    )
+}
+
+export default Header
